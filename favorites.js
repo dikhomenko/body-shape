1397184bdcac8333ca601932912130ab5c3944c0
dikhomenko <155664414+dikhomenko@users.noreply.github.com>
2024-11-13 22:08:17 +0000

    Deploying to gh-pages from @ dikhomenko/body-shape@910d8e124033662f2549b8485c721ebd5100e124 🚀

---
+++ favorites.js
@@ -0,0 +1,2 @@
+import{i,h as e,j as a,l as t,k as n}from"./assets/menu-C5DEMRyk.js";import"./assets/vendor-BcxMf0ji.js";document.addEventListener("DOMContentLoaded",()=>{i(),e(),a(),t(),n()});
+//# sourceMappingURL=favorites.js.map
